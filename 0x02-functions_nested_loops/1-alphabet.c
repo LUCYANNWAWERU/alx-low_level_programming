cf81ae79766eda2c42914591dacfad0bbe5933b8
LUCYANNWAWERU <lucyannwahitow@gmail.com>
2022-07-06 13:58:12 -0700

    Lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+
+
+
+
+/**
+* print_alphabet - prints out alphabet in lowercase
+*
+*Return: 0 if success
+*/
+
+
+
+void print_alphabet(void)
+
+
+
+{
+	int lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+	{
+		_putchar(lowercase);
+	}
+	_putchar('\n');
+	return (0);
+}
