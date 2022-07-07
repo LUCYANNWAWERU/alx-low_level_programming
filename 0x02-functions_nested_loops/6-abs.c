2af63ed2cea72dc12b92831236f384a9e26dd679
LUCYANNWAWERU <lucyannwahitow@gmail.com>
2022-07-06 17:16:13 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ *_abs -  _abs prints out the absolute value
+ *@n: interger to make absolute
+ *  Return: return the absolute value
+ */
+int  _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
