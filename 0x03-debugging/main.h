b207599563669e3e9bd703754ae5cb3ea11d9f1b
LUCYANNWAWERU <lucyannwahitow@gmail.com>
2022-07-07 14:57:55 -0700

    task 2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN.H
 #define MAIN.H
 
-int _putchar(char c);
+#include <stdio.h>
 
-void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
-int convert_day(int month, int day);
+void positive_or_negative(int i);
 void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 
-#endif
+#endif /* LUCYANN */
