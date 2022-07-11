51c77ba33124d05378ed11c49bf68fbe1b2b15c2
LUCYANNWAWERU <lucyannwahitow@gmail.com>
2022-07-11 09:30:02 -0700

    task 0

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,25 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: On success 1.
- * On error, -1 is returned, and error is set appropriately.
- */
-
-
-
-
-
-
-
 int _putchar(char c);
-
-
-
-
-
-
-
+int _isupper(int c);
 #endif
